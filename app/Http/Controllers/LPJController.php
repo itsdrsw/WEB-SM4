28ede95281a44df944c75564eda2467604f40d85
itsdrsw <marswan27@gmail.com>
2024-06-07 09:07:24 +0700

    [LPJ] Membuat konfirmasi LPJ
    Jika user menyetujui LPJ maka user cukup simpan saja ketika konfirmasi. Jika data yang masuk tidak valid, maka user diwajibkan mengungah file baru yang berisi revisi an

--- app/Http/Controllers/LPJController.php
+++ app/Http/Controllers/LPJController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LPJ;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class LPJController extends Controller
 {
@@ -26,6 +29,59 @@ class LPJController extends Controller
             ->get();
 
         return view('lpj.lpj', compact('lpj'));
+    }
+
+    public function edit($idlpj)
+    {
+        // Menggunakan join dengan eloquent
+        $lpj_ubah = LPJ::with(['progamKerja.user'])
+            ->where('idlpj', $idlpj)
+            ->firstOrFail();
+
+        // Memastikan relasi terdefinisi dalam model LPJ
+        // LPJ model harus memiliki relasi dengan ProgramKerja dan User
+        return view('lpj.lpj-ubah', [
+            'lpj_ubah' => $lpj_ubah,
+        ]);
+    }
+
+    public function update(Request $request, $idproker)
+    {
+        $validated = $request->validate([
+            // Validasi lainnya
+            'file_lpj' => 'nullable|mimes:pdf|max:3000', // Hanya file PDF dengan ukuran maksimum 3MB yang diterima
+            // 'status_proker' => 'required|in:disetujui,revisi,perbaikanrevisi', // Ubah menjadi lowercase sesuai inputan di blade
+        ]);
+
+        $proker = LPJ::findOrFail($idproker);
+
+        // Proses file lampiran proker
+        if ($request->hasFile('file_lpj')) {
+            $file = $request->file('file_lpj');
+            $fileName = $file->getClientOriginalName(); // Menggunakan nama asli file
+
+            // Menyimpan file ke folder public/sertifikat
+            $lampiranPath = $file->storeAs('file-lpj', $fileName, 'public');
+
+            // Hapus file lama jika ada
+            if ($proker->lampiran_proker) {
+                Storage::delete('public/' . $proker->file_lpj);
+            }
+
+            $validated['file_lpj'] = $lampiranPath;
+
+            // Secara otomatis mengatur status proker menjadi 'disetujui'
+            $validated['status_lpj'] = 'revisi';
+        } else {
+            // Jika tidak ada file baru diunggah, gunakan status proker yang ada dalam request
+            $validated['status_lpj'] = 'disetujui';
+        }
+
+        // Simpan perubahan dengan validasi yang sudah diatur sebelumnya
+        $proker->update($validated);
+
+        Alert::info('Success', 'Data LPJ berhasil disimpan !');
+        return redirect('/lpj');
     }
 
     public function getlpj($user_id)
---
+++ resources/views/lpj/lpj-ubah.blade.php
@@ -0,0 +1,120 @@
+@extends('template.main')
+@section('title', 'Ubah LPJ')
+@section('content')
+
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">@yield('title')</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item"><a href="/lpj">LPJ</a></li>
+                        <li class="breadcrumb-item active">@yield('title')</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="text-right">
+                                <a href="/proker" class="btn btn-warning btn-sm"><i
+                                        class="fa-solid fa-arrow-rotate-left"></i>
+                                    Back
+                                </a>
+                            </div>
+                        </div>
+                        <form class="needs-validation" novalidate action="/lpj/{{ $lpj_ubah->idlpj }}"
+                            method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="name">Nama Ormawa</label>
+                                            <input type="text" name="name"
+                                                class="form-control @error('name') is-invalid @enderror" id="name"
+                                                placeholder="Name User" value="{{ old('name', $lpj_ubah->progamKerja->user->name) }}"
+                                                readonly>
+                                            @error('name')
+                                                <span class="invalid-feedback text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label for="category">Penanggungjawab</label>
+                                            <input type="text" name="penanggung_jawab"
+                                                class="form-control @error('penanggung_jawab') is-invalid @enderror"
+                                                id="penanggung_jawab" placeholder="Masukkan nama proker"
+                                                value="{{ old('penanggung_jawab', $lpj_ubah->progamKerja->penanggung_jawab) }}"
+                                                readonly>
+                                            @error('ketua')
+                                                <span class="invalid-feedback text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label for="name">File LPJ</label>
+                                            <input type="file" name="file_lpj"
+                                                class="form-control @error('file_lpj') is-invalid @enderror"
+                                                id="file_lpj" placeholder="File" accept="application/pdf">
+                                            @error('file_lpj')
+                                                <span class="invalid-feedback text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            {{-- <label for="statusproker">Status Progam Kerja</label> --}}
+                                            <select type="hidden" style="display: none" class="custom-select" id="statusproker"
+                                                name="status_proker">
+                                                <option value="disetujui"
+                                                    {{ $lpj_ubah->status_proker == 'disetujui' ? 'selected' : '' }}>
+                                                    Disetujui
+                                                </option>
+                                                <option value="ditolak"
+                                                    {{ $lpj_ubah->status_proker == 'ditolak' ? 'selected' : '' }}>
+                                                    Ditolak
+                                                </option>
+                                            </select>
+                                            @error('status_proker')
+                                                <span class="invalid-feedback text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="card-footer text-right">
+                        <button class="btn btn-dark mr-1" type="reset"><i class="fa-solid fa-arrows-rotate"></i>
+                            Reset</button>
+                        <button class="btn btn-success" type="submit"><i class="fa-solid fa-floppy-disk"></i>
+                            Save</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+            <!-- /.content -->
+        </div>
+    </div>
+
+@endsection
